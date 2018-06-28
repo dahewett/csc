4c57ee184740e544249c497a2af86d0f1d5450cd
dhewett <dah12hewett@gmail.com>
2018-06-28 14:54:16 -0700

    CSP problem using module, Done.

--- csc421/ass3/python-constraint-master/csp_p1.py
+++ csc421/ass3/python-constraint-master/csp_p1.py
@@ -1,7 +1,12 @@
+
+
 from constraint import *
 
+# Init problem
 problem = Problem()
+# Set up domain for letters
 domain = [i for i in range(10)]
+# Addd vars
 problem.addVariables(['T', 'W', 'F', 'O', 'R', 'U'], domain)
 problem.addVariables(['C1', 'C2', 'C3'],[0, 1])
 
@@ -14,5 +19,7 @@ problem.addConstraint (lambda U, W, C1, C2: C1 + W + W == U + (10 * C2), ('U', '
 problem.addConstraint (lambda T, O, C2, C3: C2 + T + T == O + (10 * C3), ('T', 'O', 'C2', 'C3'))
 problem.addConstraint (lambda F, C3: F == C3, ('F', 'C3'))
 
+# Get solution
 sol = problem.getSolutions()
+# Print sol
 print(sol)
